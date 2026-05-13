e4077802aa4db9dcdd4f31574aff94202e1ee07b
Harlan-jpg <harlantasci437@gmail.com>
2026-05-13 09:12:02 -0600

    Added splash view code (didnt stage it last commit)

--- adet_final_y1s2/ADET_Final_iOS/ADET_Final_iOS/MoviesListView.swift
+++ adet_final_y1s2/ADET_Final_iOS/ADET_Final_iOS/MoviesListView.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct MovieListView: View {
+struct MoviesListView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    MovieListView()
+    MoviesListView()
 }
--- adet_final_y1s2/ADET_Final_iOS/ADET_Final_iOS/SplashView.swift
+++ adet_final_y1s2/ADET_Final_iOS/ADET_Final_iOS/SplashView.swift
@@ -1,22 +1,36 @@
 import SwiftUI
 
-public struct SplashView: View {
-    public init() {}
-    
-    public var body: some View {
-        ZStack {
-            Color(.systemBackground)
-                .ignoresSafeArea()
-            VStack(spacing: 16) {
-                Text("ADET Final")
-                    .font(.largeTitle).bold()
-                ProgressView()
+struct SplashView: View {
+    @State private var isActive = false
+
+    var body: some View {
+        if isActive {
+            MoviesListView()
+        } else {
+            VStack(spacing: 24) {
+                Spacer()
+                Image("AppLogo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 260, height: 260)
+                Text("Moonlight\nMotion")
+                    .multilineTextAlignment(.center)
+                    .font(.system(size: 32, weight: .light))
+                    .tracking(8)
+                    .foregroundColor(Color(red: 0.10, green: 0.16, blue: 0.29))
+                Spacer()
+                Text("Created by: Harlan Tasci")
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+                    .padding(.bottom, 40)
             }
-            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.white.ignoresSafeArea())
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    withAnimation { isActive = true }
+                }
+            }
         }
     }
-}
-
-#Preview {
-    SplashView()
 }
